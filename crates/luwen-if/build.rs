3ae6aec314ddcd327d736025b8adba4a1d1f5a50
Daniel Rosen <drosen@tenstorrent.com>
2025-03-10 16:50:51 -0400

    Attempt to use default protoc compiler before falling back to vendored
    Signed-off-by: Daniel Rosen <drosen@tenstorrent.com>

--- crates/luwen-if/build.rs
+++ crates/luwen-if/build.rs
@@ -1,21 +1,47 @@
-use std::{env, fs};
+use std::{env, fs, path::PathBuf};
 
-fn compiled_proto_file_by_name(
+fn try_to_compiled_proto_file_by_name(
     name: &str,
+    protoc_path: Option<PathBuf>,
     out_dir: &str,
-) -> std::result::Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), std::io::Error> {
     let proto_file = format!("{}.proto", name);
     let outname = format!("{}/{}.rs", out_dir, name);
-    prost_build::Config::new()
-        .protoc_executable(protoc_bin_vendored::protoc_bin_path().unwrap())
-        .out_dir(out_dir)
-        .type_attribute(".", "#[derive(serde::Serialize)]") // Add `#[derive(Serialize)]` to all generated messages for easy HashMap conversion
-        .compile_protos(&[proto_file], &["bh_spirom_protobufs/"])?;
+    let mut protoc_build_config = prost_build::Config::new();
+    protoc_build_config.out_dir(out_dir);
+
+    // Add `#[derive(Serialize)]` to all generated messages for easy HashMap conversion
+    protoc_build_config.type_attribute(".", "#[derive(serde::Serialize)]");
+
+    if let Some(protoc_path) = protoc_path {
+        protoc_build_config.protoc_executable(protoc_path);
+    }
+
+    protoc_build_config.compile_protos(&[proto_file], &["bh_spirom_protobufs/"])?;
     fs::rename(format!("{}/_.rs", out_dir), outname)?;
     Ok(())
 }
 
-fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+fn compiled_proto_file_by_name(name: &str, out_dir: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let first_try = try_to_compiled_proto_file_by_name(name, None, out_dir);
+    match first_try {
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+            // If we couldn't find the system protoc then use the vendored
+            try_to_compiled_proto_file_by_name(
+                name,
+                Some(protoc_bin_vendored::protoc_bin_path()?),
+                out_dir,
+            )?;
+        }
+        other => {
+            other?;
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Get the output directory from Cargo
     let out_dir = env::var("OUT_DIR")?;
     compiled_proto_file_by_name("fw_table", &out_dir)?;
@@ -24,4 +50,3 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
-
