cdc7d0ffe334458bd8df01ca1c9e3d9a7b026f7b
Daniel Rosen <drosen@tenstorrent.com>
2025-03-10 16:33:06 -0400

    Use vendored version of the protbuf compiler during build
    Signed-off-by: Daniel Rosen <drosen@tenstorrent.com>

--- crates/luwen-if/build.rs
+++ crates/luwen-if/build.rs
@@ -1,9 +1,13 @@
 use std::{env, fs};
 
-fn compiled_proto_file_by_name(name: &str, out_dir: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+fn compiled_proto_file_by_name(
+    name: &str,
+    out_dir: &str,
+) -> std::result::Result<(), Box<dyn std::error::Error>> {
     let proto_file = format!("{}.proto", name);
     let outname = format!("{}/{}.rs", out_dir, name);
     prost_build::Config::new()
+        .protoc_executable(protoc_bin_vendored::protoc_bin_path().unwrap())
         .out_dir(out_dir)
         .type_attribute(".", "#[derive(serde::Serialize)]") // Add `#[derive(Serialize)]` to all generated messages for easy HashMap conversion
         .compile_protos(&[proto_file], &["bh_spirom_protobufs/"])?;
@@ -20,3 +24,4 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
