455f0f373acf3094318a028e2f06e3861d300cec
Zakhary Kaplan <zkaplan@tenstorrent.com>
2025-10-29 14:40:03 -0400

    refactor(core): improve ergonomics for `Arch`
    Signed-off-by: Zakhary Kaplan <zkaplan@tenstorrent.com>

--- crates/luwen-core/src/lib.rs
+++ crates/luwen-core/src/lib.rs
@@ -4,20 +4,19 @@
 use std::fmt;
 use std::str::FromStr;
 
-#[derive(Clone, Hash, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Arch {
     Grayskull,
+    #[default]
     Wormhole,
     Blackhole,
 }
 
-impl Default for Arch {
-    fn default() -> Self {
-        Self::Grayskull
-    }
-}
-
 impl Arch {
+    pub fn is_grayskull(&self) -> bool {
+        matches!(self, Arch::Grayskull)
+    }
+
     pub fn is_wormhole(&self) -> bool {
         matches!(self, Arch::Wormhole)
     }
@@ -35,7 +34,7 @@ impl FromStr for Arch {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
+        match s.to_lowercase().as_str() {
             "grayskull" => Ok(Arch::Grayskull),
             "wormhole" => Ok(Arch::Wormhole),
             "blackhole" => Ok(Arch::Blackhole),
@@ -46,10 +45,6 @@ impl FromStr for Arch {
 
 impl fmt::Display for Arch {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Arch::Grayskull => write!(f, "Grayskull"),
-            Arch::Wormhole => write!(f, "Wormhole"),
-            Arch::Blackhole => write!(f, "Blackhole"),
-        }
+        write!(f, "{self:?}")
     }
 }
