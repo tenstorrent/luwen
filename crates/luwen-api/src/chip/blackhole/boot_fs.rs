b86d50aa26995562b29aa90355109d812e24c764
Andrew Lewycky <alewycky@tenstorrent.com>
2026-02-11 11:20:05 -0500

    boot_fs improvements

--- crates/luwen-api/src/chip/blackhole/boot_fs.rs
+++ crates/luwen-api/src/chip/blackhole/boot_fs.rs
@@ -50,6 +50,7 @@ pub struct TtBootFsHeader {
     pub magic: u32,
     pub version: u32,
     pub num_fds: u32,
+    // descriptor_table_addrs: [u32; num_fds]
 }
 
 impl TtBootFsFd {
@@ -82,62 +83,52 @@ impl fmt::Debug for TtBootFsFd {
     }
 }
 
+fn read_pod<T: Pod>(reader: impl Fn(u32, usize) -> Vec<u8>, addr: u32) -> Option<T> {
+    let bytes = reader(addr, mem::size_of::<T>());
+    bytes.get(..mem::size_of::<T>()).map(bytemuck::pod_read_unaligned::<T>)
+}
+
 pub fn read_fd(reader: impl Fn(u32, usize) -> Vec<u8>, addr: u32) -> Option<TtBootFsFd> {
-    let fd_bytes = reader(addr, mem::size_of::<TtBootFsFd>());
-    if fd_bytes.len() == mem::size_of::<TtBootFsFd>() {
-        Some(unsafe {
-            mem::transmute::<[u8; mem::size_of::<TtBootFsFd>()], TtBootFsFd>(
-                fd_bytes.try_into().unwrap(),
-            )
-        })
-    } else {
-        None
-    }
+    read_pod(reader, addr)
 }
 
 pub fn read_header(reader: impl Fn(u32, usize) -> Vec<u8>, addr: u32) -> Option<TtBootFsHeader> {
-    let header_bytes = reader(addr, mem::size_of::<TtBootFsHeader>());
-    if header_bytes.len() == mem::size_of::<TtBootFsHeader>() {
-        Some(unsafe {
-            mem::transmute::<[u8; mem::size_of::<TtBootFsHeader>()], TtBootFsHeader>(
-                header_bytes.try_into().unwrap(),
-            )
-        })
-    } else {
-        None
+    read_pod(reader, addr)
+}
+
+fn find_descriptor_tables(reader: impl Fn(u32, usize) -> Vec<u8>) -> Vec<u32> {
+    let mut descriptor_table_addrs = Vec::new();
+
+    if let Some(header) = read_header(&reader, BOOT_FS_HEADER_START) {
+        if header.magic == BOOT_FS_HEADER_MAGIC {
+            let descriptor_table_list_addr = BOOT_FS_HEADER_START + mem::size_of::<TtBootFsHeader>() as u32;
+            let descriptor_table_raw = reader(descriptor_table_list_addr, (header.num_fds * 4) as usize);
+            for addr_raw in descriptor_table_raw.chunks_exact(4) {
+                let descriptor_table_addr = u32::from_le_bytes(addr_raw.try_into().unwrap());
+                descriptor_table_addrs.push(descriptor_table_addr);
+            }
+        } else {
+            // Legacy bootfs. Just has tables at 0x0 and 0x4000
+            descriptor_table_addrs = vec![0x0, 0x4000];
+        }
     }
+    descriptor_table_addrs
 }
 
 pub fn read_tag(reader: impl Fn(u32, usize) -> Vec<u8>, tag: &str) -> Option<(u32, TtBootFsFd)> {
-    let header_addr = BOOT_FS_HEADER_START; // Boot FS header start address
-    let mut curr_addr;
-    let header = read_header(&reader, header_addr).unwrap();
-    let mut boot_headers: Vec<u32> = Vec::new();
-    if header.magic != BOOT_FS_HEADER_MAGIC {
-        // Legacy bootfs. Just has headers at 0x0 and 0x4000
-        boot_headers = vec![0x0, 0x4000];
-    } else {
-        curr_addr = header_addr + mem::size_of::<TtBootFsHeader>() as u32;
-        // Read through the number of FDs specified in the header
-        for _ in 0..header.num_fds {
-            // Read a u32 address of each FD
-            let addr_bytes = reader(curr_addr, 4);
-            let fd_addr = u32::from_le_bytes(addr_bytes.try_into().unwrap());
-            boot_headers.push(fd_addr);
-            curr_addr += 4;
-        }
-    }
-    for addr in boot_headers {
-        curr_addr = addr;
+    let boot_headers = find_descriptor_tables(&reader);
+
+    for table_addr in boot_headers {
+        let mut fd_addr = table_addr;
         loop {
-            let fd = read_fd(&reader, curr_addr).unwrap();
+            let fd = read_fd(&reader, fd_addr).unwrap();
             if fd.flags.invalid() {
                 break;
             }
             if fd.image_tag_str() == tag {
-                return Some((addr, fd));
+                return Some((fd_addr, fd));
             }
-            curr_addr += mem::size_of::<TtBootFsFd>() as u32;
+            fd_addr += mem::size_of::<TtBootFsFd>() as u32;
         }
     }
     None
